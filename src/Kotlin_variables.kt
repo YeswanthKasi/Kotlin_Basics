a93ad4c1a4b9bf0f3b8591262c2423a0171f0381
Yeswanth <kasireddiyeswanth29@gmail.com>
2025-02-07 15:23:19 +0530

    Kotlin_variables.kt added the yml file

---
+++ src/Kotlin_variables.kt
@@ -0,0 +1,3 @@
+var name = "Yeswanth"
+val lastName = "Kasireddi"
+//The difference between var and val is that variables declared with the var keyword can be changed/modified, while val variables cannot.
