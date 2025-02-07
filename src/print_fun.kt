e0cb47bccbb79e5400b4d303d5090f94180d2b46
Yeswanth <kasireddiyeswanth29@gmail.com>
2025-02-07 15:05:55 +0530

    print_fun.kt added the print function with println

--- src/print_fun.kt
+++ src/print_fun.kt
@@ -3,4 +3,5 @@ fun main() {
     println("Kasireddi")
     print("Hello Yeswanth\n") //Does not add the new line for this command
     println("I am learning Kotlin")
+    /*,"/**/"This is the Multiline comment which can be added to the project in multiple lines */
 }
